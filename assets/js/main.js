3e23efeb7a4937773d26559fc6dd8e01f1cc625c
iqbalthex <iqbalariem232@gmail.com>
2023-02-15 17:03:42 +0700

    go to another section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,71 @@
-const alertEl = document.querySelector('.alert');
-let supportedViewport = true;
+const menuDropdown = document.querySelector('nav .menu-dropdown');
+
 
 window.onload = function() {
-  document.querySelectorAll('link[media=print]').forEach(link => link.media = 'all');
-  document.querySelectorAll('.head input[type=radio]').forEach(input => makeContent(input));
 
-  setTimeout(() => {
-    document.querySelectorAll('.content').forEach(cont => cont.style.display = 'block');
-  }, 950);
-  setTimeout(() => {
-    document.querySelector('.content.about').classList.add('active');
-    decorActive();
-  }, 1000);
-}
+document.querySelectorAll('link[media=print]').forEach(link => link.media = 'all');
+document.querySelectorAll('.head input[type=radio]').forEach(input => makeContent(input));
 
-window.onresize = function() {
-  supportedViewport = window.innerWidth > 360 && window.innerWidth < 600;
-  if (!supportedViewport) {
-    aleret();
-  }
+document.addEventListener('click', changeDropdownState);
+
+setTimeout(() => document.querySelectorAll('.content')
+  .forEach(cont => cont.style.display = 'block'), 950);
+
+setTimeout(() => {
+  document.querySelector('.content.about').classList.add('active');
+  decorActive();
+}, 1000);
+
 }
 
 
-function aleret() {
-  alertEl.style.animation = 'alert 1.5s';
-  setTimeout(() => {
-    alertEl.style.animation = 'unset';
-  }, 2500);
+function changeDropdownState(event) {
+
+const toggler = document.querySelector('.menu-toggler input[type=checkbox]');
+const span = document.querySelector('.menu-toggler span');
+
+if ((event.target === toggler) && toggler.checked) {
+  span.innerText = 'X';
+  return menuDropdown.classList.add('dropdown-expanded');
 }
+
+span.innerText = '=';
+toggler.checked = false;
+menuDropdown.classList.remove('dropdown-expanded');
+
+}
+
 
 function makeContent(input) {
-  const contName = input.id.split('-')[0];
-  const cont = document.querySelector(`.content.${contName}`);
 
-  input.onchange = function() {
-    this.checked && this.activate();
-    decorActive();
-  }
+const contName = input.id.split('-')[0];
+const cont = document.querySelector(`.content.${contName}`);
 
-  input.activate = function() {
-    document.querySelectorAll(`.content:not(.${contName})`)
-      .forEach(cont => cont.classList.remove('active'));
+input.onchange = function() {
+  this.checked && this.activate();
+  decorActive();
+}
 
-    cont.classList.add('active');
-  }
+input.activate = function() {
+  document.querySelectorAll(`.content:not(.${contName})`)
+    .forEach(cont => cont.classList.remove('active'));
 
-  return cont;
+  cont.classList.add('active');
+}
+
+return cont;
+
 }
 
 
 function decorActive() {
-  const label = document.querySelector('.active label > span');
-  let html = '';
-  for (let i = 0; i < label.innerText.length; i++) {
-    const delay = .1 * i;
-    html += `<span style="--delay: ${delay}s">${label.innerText[i]}</span>`;
-  }
-  label.innerHTML = html;
+
+const label = document.querySelector('.active label > span');
+let html = '';
+for (let i = 0; i < label.innerText.length; i++) {
+  const delay = .1 * i;
+  html += `<span style="--delay: ${delay}s">${label.innerText[i]}</span>`;
+}
+label.innerHTML = html;
+
 }
